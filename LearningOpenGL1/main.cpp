f8e14cc5e19015a6d4f9707d890ca154c88654b5
Ahmad Ayman <mongez.me@gmail.com>
2017-07-08 22:48:32 +0200

    Day1 : Opening a window.

---
+++ LearningOpenGL1/main.cpp
@@ -0,0 +1,70 @@
+#include <glad\glad.h>			// include glad before glfw as it contain the opengl headers 
+#include <GLFW\glfw3.h>
+#include <iostream>
+
+// register a callback function on the window to be called each time the window get resized
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+	glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow *window)
+{
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE))
+	{
+		glfwSetWindowShouldClose(window, true);
+	}
+}
+
+int main()
+{
+	glfwInit();
+	// set hints fot the next call of GLFWCreateWindow() call
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	//glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+	GLFWwindow* window = glfwCreateWindow(800, 600, "First Window", NULL, NULL);
+	if (window == NULL)
+	{
+		std::cout << "ERROR::Faild to create GLFW Window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+
+	// Init GLAD
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		std::cout << "Failed to initialize GLAD" << std::endl;
+		return -1;
+	}
+
+	glViewport(0, 0, 800, 600);	// tell opengl the size of the rendering window
+
+	glfwSetWindowSizeCallback(window, framebuffer_size_callback);
+
+	// Rendering loop
+	while (!glfwWindowShouldClose(window))
+	{
+		// 1- pull events
+		// 2- render staff
+		// 3- swap buffers (display the renders)
+
+		processInput(window);
+
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+		glfwSwapBuffers(window);		// state-setting function
+		glfwPollEvents();				// state-using function
+	}
+
+	glfwTerminate();		// deallocate the resources 
+
+	return 0;
+}
+
+
+
